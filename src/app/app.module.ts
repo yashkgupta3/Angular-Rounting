6d6227dd4a135ce0e2a82310f9ebc19970537636
yashk <yashkgupta3@gmail.com>
2022-08-27 20:52:12 -0700

    intiial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { UserComponent } from './user/user.component';
+import { AboutComponent } from './about/about.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    UserComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
